40d8180ce64754e7c4f956a1b911f3222b98c503
Misha Awad <awadmishaa@gmail.com>
2024-02-13 19:26:14 -0500

    add dura point feature to layers object in layers.js

---
+++ layers.js
@@ -0,0 +1,19 @@
+var layers = {
+    features : [{
+        duras_location : {
+            'type': 'geojson',
+            'data': {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [ 4534026.621355312876403, 4129746.456178490072489 ]
+            },
+            'properties': {
+                'title': 'Dura-Europos',
+                'marker-symbol': ''
+                }
+            }
+        }]       
+    };
+
+
