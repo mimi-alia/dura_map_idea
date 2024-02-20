005eb4975227b00e854f7e431653004340715631
Misha Awad <awadmishaa@gmail.com>
2024-02-20 09:59:46 -0500

    unsure

--- config.js
+++ config.js
@@ -102,10 +102,15 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-            {
-                layer: 'trade_routes',
-                opacity: 1,
-            },
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+                {
+                    layer: 'trade_routes',
+                    opacity: 1,
+                },
+            
             ],
             onChapterExit: [
                 {
@@ -134,19 +139,28 @@ var config = {
                 {
                     layer: 'seleucid_empire',
                     opacity: 0.5,
-                }
+                },
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
             ],
-            onChapterExit: []
+            onChapterExit: [
+                {
+                    layer: 'seleucid_empire',
+                    opacity: 0,
+                },
+            ]
         },
         {
             id: 'roman-territory-300bce',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
             title: '',
             image: '',
             description: 'On the other side of the Mediterranean sits the Roman Empire whose territory would steadily grow over the course of the next centuries.',
             location: {
-                center: [47.55553, 36.93892],
+                center: [42.19101, 36.93892],
                 zoom: 4.32,
                 pitch: 0.00,
                 bearing: 0.00
@@ -154,8 +168,23 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+                {
+                    layer: 'roman_territory_70ce',
+                    opacity: .5,
+                },
+
+            ],
+            onChapterExit: [
+                {
+                    layer: 'roman_territory_70ce',
+                    opacity: 0,
+                },
+            ]
         },
         {
             id: 'parthian-territory-100bce',
@@ -165,7 +194,7 @@ var config = {
             image: '',
             description: 'By the late second century BCE, Dura came under the control of the growing Arsacid (Parthian) Empire.',
             location: {
-                center: [47.55553, 36.93892],
+                center: [40.76208, 34.62198],
                 zoom: 4.32,
                 pitch: 0.00,
                 bearing: 0.00
@@ -173,18 +202,32 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+                {
+                    layer: 'parthian_territory',
+                    opacity: .5,
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'parthian_territory',
+                    opacity: 0, 
+                },
+            ]
         },
         {
             id: 'roman-territory-165ce',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
             title: '',
             image: '',
             description: 'By the late 2nd century CE, the site had been absorbed into the territory of the Roman Empire to the West.',
             location: {
-                center: [47.55553, 36.93892],
+                center: [40.76208, 34.62198],
                 zoom: 4.32,
                 pitch: 0.00,
                 bearing: 0.00
@@ -192,8 +235,22 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+                {
+                    layer: 'roman_territory_200ce',
+                    opacity: .5,
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'roman_territory_200ce',
+                    opacity: 0,
+                },
+            ]
         },
         {
             id: 'sasanian-territory-256ce',
@@ -203,206 +260,233 @@ var config = {
             image: '',
             description: 'Dura\’s ancient occupation came to an end around 256 CE, when it was one of a number of Roman cities besieged and conquered by the nascent Sasanian Persian Empire as it expanded its territory westward.',
             location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
+                center: [40.76208, 34.62198],
+                zoom: 5.47,
                 pitch: 0.00,
                 bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+                {
+                    layer: 'sassanid_territory_250sce',
+                    opacity: .5,
+                },
+                {
+                    layer: 'roman_relinquished_3ce',
+                    opacity: .5,
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'sassanid_territory_250sce',
+                    opacity: 0,
+                },
+                {
+                    layer: 'roman_relinquished_3ce',
+                    opacity: 0,
+                },
+            ]
         },
         {
             id: 'dura-europos-fall',
-            alignment: 'left',
+            alignment: 'center',
             hidden: false,
             title: '',
             image: '',
-            description: 'But history didn’t stop when Dura-Europos fell.',
+            description: 'But history didn\’t stop when Dura-Europos fell.',
             location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
+                center: [40.76208, 34.62198],
+                zoom: 5.47,
                 pitch: 0.00,
                 bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
+            onChapterEnter: [
+                {
+                    layer: 'dura_location',
+                    opacity: 1,
+                },
+            ],
             onChapterExit: []
         },
-        {
-            id: 'sasanian-empire-620ce',
-            alignment: 'left',
-            hidden: false,
-            title: 'c. 620 CE Sasanian Empire',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'ummayad-caliphate-720ce',
-            alignment: 'left',
-            hidden: false,
-            title: 'c. 720 CE Ummayad Caliphate',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'abbasid-caliphate-720ce',
-            alignment: 'left',
-            hidden: false,
-            title: 'c.  850 CE Abbasid Caliphate',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'seljuk-empire',
-            alignment: 'left',
-            hidden: false,
-            title: '[date]Seljuk Empire',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'ottoman-empire',
-            alignment: 'left',
-            hidden: false,
-            title: ' [date]  Ottoman Empire right before WWI',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'ottoman-empire-desc',
-            alignment: 'left',
-            hidden: false,
-            title: '',
-            image: '',
-            description: 'As one of the central powers defeated in WWI, the Ottoman Empire came to an end and forfeited its territory.',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'french-british-mandate',
-            alignment: 'left',
-            hidden: false,
-            title: '',
-            image: '',
-            description: 'In the power vacuum after the forfeiture of Ottoman territory, the French and British stymied efforts to establish a self-governed Arab state and instead established Mandate governments divided along the Sykes-Picot line.',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'french-mandate-impact',
-            alignment: 'left',
-            hidden: false,
-            title: '',
-            image: '',
-            description: 'It was under French Mandate authority that largescale excavation campaigns employing hundreds of local laborers were carried out at Dura-Europos through the 1920s and 30s. Power dynamics and labor relations of the era left local contributions largely uncredited.',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'modern-french-presence',
-            alignment: 'left',
-            hidden: false,
-            title: 'French military presence until 1946.',
-            image: '',
-            description: '',
-            location: {
-                center: [47.55553, 36.93892],
-                zoom: 4.32,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
+        // {
+        //     id: 'sasanian-empire-620ce',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: 'c. 620 CE Sasanian Empire',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'ummayad-caliphate-720ce',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: 'c. 720 CE Ummayad Caliphate',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'abbasid-caliphate-720ce',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: 'c.  850 CE Abbasid Caliphate',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'seljuk-empire',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: '[date]Seljuk Empire',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'ottoman-empire',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: ' [date]  Ottoman Empire right before WWI',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'ottoman-empire-desc',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: '',
+        //     image: '',
+        //     description: 'As one of the central powers defeated in WWI, the Ottoman Empire came to an end and forfeited its territory.',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'french-british-mandate',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: '',
+        //     image: '',
+        //     description: 'In the power vacuum after the forfeiture of Ottoman territory, the French and British stymied efforts to establish a self-governed Arab state and instead established Mandate governments divided along the Sykes-Picot line.',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'french-mandate-impact',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: '',
+        //     image: '',
+        //     description: 'It was under French Mandate authority that largescale excavation campaigns employing hundreds of local laborers were carried out at Dura-Europos through the 1920s and 30s. Power dynamics and labor relations of the era left local contributions largely uncredited.',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        // {
+        //     id: 'modern-french-presence',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: 'French military presence until 1946.',
+        //     image: '',
+        //     description: '',
+        //     location: {
+        //         center: [47.55553, 36.93892],
+        //         zoom: 4.32,
+        //         pitch: 0.00,
+        //         bearing: 0.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
     ]
 }
